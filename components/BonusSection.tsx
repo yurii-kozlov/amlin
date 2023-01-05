96b0d46234ee112d897f606e10fad47fb2d92a1f
Yurii Kozlov <yurka22870@gmail.com>
2023-01-06 01:01:15 +0200

    bonus section was improved

--- components/BonusSection.tsx
+++ components/BonusSection.tsx
@@ -2,14 +2,20 @@ import Link from 'next/link';
 import React, { ReactElement } from 'react';
 import styles from 'styles/layout/BonusSection.module.scss';
 import classes from 'styles/base/container.module.scss';
+import Image from 'next/image';
+import zipLogo from 'images/icons/zipLogo.svg';
 
 export const BonusSection: React.FC = (): ReactElement => (
   <div className={classes.container} >
     <section className={styles.section} >
-      <span className={styles.logo} />
+      <Image
+        alt='zip-logo'
+        className={styles.logo}
+        src={zipLogo}
+      />
       <p className={styles.description}><strong>own</strong> it now, up to 6 months interest free
       </p>
       <Link className={styles.moreInfoLink} href="#">learn more</Link>
     </section>
   </div>
-)
+);

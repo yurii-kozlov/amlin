3cfac50e20ddee4703b5f090142e6c369e138453
Yurii Kozlov <yurka22870@gmail.com>
2023-01-06 01:12:19 +0200

    eslint disabling was deleted

--- api/paymentSystemsImages.ts
+++ api/paymentSystemsImages.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import paypal from 'images/icons/paymentSystems/paypal.svg';
 import visa from 'images/icons/paymentSystems/visa.svg';
 import maestro from 'images/icons/paymentSystems/maestro.svg';

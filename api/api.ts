a0149003143996dece1a7eedd7fa7d18accb8faa
Yurii Kozlov <yurka22870@gmail.com>
2023-01-23 12:59:50 +0200

    fetch functions were created

---
+++ api/api.ts
@@ -0,0 +1,19 @@
+import axios, {AxiosResponse} from 'axios';
+
+function get<T>(url:string):Promise<T> {
+  const fullURL = process.env.BASE_URL + url;
+
+  return axios.get(fullURL);
+}
+
+export const getLaptops = (currentPage: number): Promise<AxiosResponse> => (
+  get<AxiosResponse>(`/laptops?_limit=10&_page=${currentPage}`)
+);
+
+export const getMonitors = (currentPage: number): Promise<AxiosResponse> => (
+  get<AxiosResponse>(`/monitors?_limit=10&_page=${currentPage}`)
+);
+
+export const getComputers = (currentPage: number): Promise<AxiosResponse> => (
+  get<AxiosResponse>(`/computers?_limit=10&_page=${currentPage}`)
+);

3e77f752050fcc96a145bb54f64a8b5965d4252e
Yurii Kozlov <yurka22870@gmail.com>
2023-01-26 13:36:17 +0200

    loader mark-up was improved

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,4 @@
 import React, { ReactElement }from 'react';
-import { Container } from 'components/Container';
 import styles from 'styles/layout/Loader.module.scss';
 
-export const Loader = (): ReactElement => (
-  <Container>
-    <div className={styles.loader} />
-  </Container>
-  );
+export const Loader = (): ReactElement => <div className={styles.loader} />;

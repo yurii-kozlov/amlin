35f70d651c4873a5fccc262e58084fb61a0dcce5
Yurii Kozlov <yurka22870@gmail.com>
2023-02-02 12:13:34 +0200

    slider responsiveness was improved

--- constants/newProductsCarouselConstants.tsx
+++ constants/newProductsCarouselConstants.tsx
@@ -9,37 +9,33 @@ export const settingsOnDesktop = {
   slidesToShow: 4,
   slidesToScroll: 1,
   centerMode: true,
+  responsive: [
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 2,
+        centerPadding: '5px',
+        arrows: false
+      },
+    },
+    {
+      breakpoint: 690,
+      settings: {
+        slidesToShow: 2,
+        centerPadding: '30px',
+        arrows: false
+      },
+    },
+    {
+      breakpoint: 1030,
+      settings: {
+        slidesToShow: 3,
+        centerPadding: '30px',
+        arrows: false
+      },
+    }
+  ],
   centerPadding: '50px',
-  nextArrow: <SampleNextArrow />,
-  prevArrow: <SamplePrevArrow />
-};
-
-export const settingsOnTablet = {
-  dots: false,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 10000,
-  speed: 1000,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  centerMode: true,
-  centerPadding: '30px',
-  arrows: false,
-  nextArrow: <SampleNextArrow />,
-  prevArrow: <SamplePrevArrow />
-};
-
-export const settingsOnMobile = {
-  dots: false,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 10000,
-  speed: 1000,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  centerMode: true,
-  centerPadding: '5px',
-  arrows: false,
   nextArrow: <SampleNextArrow />,
   prevArrow: <SamplePrevArrow />
 };

113d0f417c83a3b23cf244bd6c82f6fcfff8e0a8
Yurii Kozlov <yurka22870@gmail.com>
2023-02-17 01:05:43 +0200

    code styling for clearCart testing was improved

--- __tests__/store/clearCart.test.ts
+++ __tests__/store/clearCart.test.ts
@@ -4,9 +4,7 @@ import { mockProducts } from '__mocks__/mockStoreData';
 const { laptop, computer, laptop2 } = mockProducts;
 
 test('Cart is empty after clearing', () => {
-  personalAccount.addProductToCart(laptop, 4);
-  personalAccount.addProductToCart(laptop2, 3);
-  personalAccount.addProductToCart(computer);
+  personalAccount.cart = [laptop, computer, laptop2]
   personalAccount.clearCart();
 
   expect(personalAccount.cart.length).toBe(0);

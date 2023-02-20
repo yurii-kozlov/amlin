2bfd8b43c948a98a2031e8fd0d20e78d23c55bc5
Yurii Kozlov <yurka22870@gmail.com>
2023-02-20 17:49:35 +0200

    code styling for testing data was improved

--- cypress/testingData/E2EtestingData.ts
+++ cypress/testingData/E2EtestingData.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 export const E2EtestingProducts = {
   laptop: {
     'inStock': true,
@@ -10,19 +9,27 @@ export const E2EtestingProducts = {
     'previousPrice': 29999,
     'price': 21499,
     'rating': 3,
-    'description': '15.6 inch FHD 4 way NanoEdge bezel display with a stunning 88% screen-to-body ratio\nPowerful AMD Quad Core Ryzen 5 3500U Processor (2M Cache, up to 3.6 GHz)\nAMD Radeon Vega 8 discrete graphics with Windows 10 Home\n8GB DDR4 RAM and 256GB PCIe NVMe M.2 SSD\nErgonomic backlit keyboard with fingerprint sensor activated via Windows Hello\nExclusive ErgoLift design for improved typing position'
+    'description': '15.6 inch FHD 4 way NanoEdge bezel display with a stunning 88% screen-to-body' +
+      'ratio\nPowerful AMD Quad Core Ryzen 5 3500U Processor (2M Cache, up to 3.6 GHz)\nAMD Radeon Vega' +
+      '8 discrete graphics with Windows 10 Home\n8GB DDR4 RAM and 256GB PCIe NVMe M.2 SSD\nErgonomic' +
+      'backlit keyboard with fingerprint sensor activated via Windows Hello\nExclusive ErgoLift design' +
+      'for improved typing position'
   },
   computer: {
     'inStock': true,
     'slug': 'X65v12',
     'firm': 'Artline',
     'url': 'https://res.cloudinary.com/dqdonqby4/image/upload/v1612528866/new-products/9_uhvi0t.jpg',
-    'name': 'Computer  ARTLINE Gaming X65 (X65v12)',
+    'name': 'Computer ARTLINE Gaming X65 (X65v12)',
     'reviewsCount': 32,
     'previousPrice': 23933,
     'price': 17856,
     'rating': 3.3,
-    'description': '15.6 inch FHD 4 way NanoEdge bezel display with a stunning 88% screen-to-body ratio\nPowerful AMD Quad Core Ryzen 5 3500U Processor (2M Cache, up to 3.6 GHz)\nAMD Radeon Vega 8 discrete graphics with Windows 10 Home\n8GB DDR4 RAM and 256GB PCIe NVMe M.2 SSD\nErgonomic backlit keyboard with fingerprint sensor activated via Windows Hello\nExclusive ErgoLift design for improved typing position'
+    'description': '15.6 inch FHD 4 way NanoEdge bezel display with a stunning 88% screen-to-body' +
+      'ratio\nPowerful AMD Quad Core Ryzen 5 3500U Processor (2M Cache, up to 3.6 GHz)\nAMD Radeon' +
+      'Vega 8 discrete graphics with Windows 10 Home\n8GB DDR4 RAM and 256GB PCIe NVMe M.2' +
+      ' SSD\nErgonomic backlit keyboard with fingerprint sensor activated via Windows Hello\nExclusive' +
+      'ErgoLift design for improved typing position'
   },
   monitor: {
     'inStock': true,
@@ -34,6 +41,10 @@ export const E2EtestingProducts = {
     'previousPrice': 5933,
     'price': 4499,
     'rating': 2.8,
-    'description': '15.6 inch FHD 4 way NanoEdge bezel display with a stunning 88% screen-to-body ratio\nPowerful AMD Quad Core Ryzen 5 3500U Processor (2M Cache, up to 3.6 GHz)\nAMD Radeon Vega 8 discrete graphics with Windows 10 Home\n8GB DDR4 RAM and 256GB PCIe NVMe M.2 SSD\nErgonomic backlit keyboard with fingerprint sensor activated via Windows Hello\nExclusive ErgoLift design for improved typing position'
+    'description':'15.6 inch FHD 4 way NanoEdge bezel display with a stunning 88% screen-to-body' +
+      'ratio\nPowerful AMD Quad Core Ryzen 5 3500U Processor (2M Cache, up to 3.6 GHz)\nAMD Radeon' +
+      'Vega 8 discrete graphics with Windows 10 Home\n8GB DDR4 RAM and 256GB PCIe NVMe M.2' +
+      'SSD\nErgonomic backlit keyboard with fingerprint sensor activated via Windows Hello\nExclusive' +
+      'ErgoLift design for improved typing position'
   },
 }
